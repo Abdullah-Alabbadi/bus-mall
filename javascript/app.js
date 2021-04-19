e10cc37cd8e919434f871d6130254dedbfc0222e
Abdullah Alabbadi <abdullah_alabbadi2018@yahoo.com>
2021-04-19 14:46:57 +0300

    Fixing some problem in the code

--- javascript/app.js
+++ javascript/app.js
@@ -3,13 +3,13 @@
 let webGallay1 = document.getElementById('webGallary1L');
 let webGallay2 = document.getElementById('webGallary2C');
 let webGallay3 = document.getElementById('webGallary3R');
-
-
+let secOne = document.getElementById('sec-one');
+let Result = document.getElementById('Result');
 let leftWebGallaryIndex1;
 let centerWebGallaryIndex2;
 let rightWebGallaryIndex3;
 let count = 0;
-let maxAttempts = 25;
+let maxAttempts = 5;
 
 function Render(Name, source) {
   this.name = Name;
@@ -76,28 +76,29 @@ function collectImageToGenerate() {
   centerWebGallaryIndex2 = generateRandomImag();
   rightWebGallaryIndex3 = generateRandomImag();
 
-}
+  //no repeated image
+  while ((leftWebGallaryIndex1 === centerWebGallaryIndex2 || leftWebGallaryIndex1 === rightWebGallaryIndex3 || rightWebGallaryIndex3 === centerWebGallaryIndex2 || rightWebGallaryIndex3 === leftWebGallaryIndex1)) {
+    leftWebGallaryIndex1 = generateRandomImag();
+    centerWebGallaryIndex2 = generateRandomImag();
+  }
+  // console.log(leftWebGallaryIndex1);
 
-//no repeated image
-while ((leftWebGallaryIndex1 === centerWebGallaryIndex2 || leftWebGallaryIndex1 === rightWebGallaryIndex3 || rightWebGallaryIndex3 === centerWebGallaryIndex2 || rightWebGallaryIndex3 === leftWebGallaryIndex1)) {
-  leftWebGallaryIndex1 = generateRandomImag();
-  centerWebGallaryIndex2 = generateRandomImag();
-}
-// console.log(leftWebGallaryIndex1);
+  // displaying the images
+  webGallay1.src = Render.allElements[leftWebGallaryIndex1].source;
+  Render.allElements[leftWebGallaryIndex1].showSelected++;
+  webGallay2.src = Render.allElements[centerWebGallaryIndex2].source;
+  Render.allElements[centerWebGallaryIndex2].showSelected++;
+  webGallay3.src = Render.allElements[rightWebGallaryIndex3].source;
+  Render.allElements[rightWebGallaryIndex3].showSelected++;
 
-// displaying the images
-webGallay1.src = Render.allElements[leftWebGallaryIndex1].source;
-Render.allElements[leftWebGallaryIndex1].showSelected++;
-webGallay2.src = Render.allElements[centerWebGallaryIndex2].source;
-Render.allElements[centerWebGallaryIndex2].showSelected++;
-webGallay3.src = Render.allElements[rightWebGallaryIndex3].source;
-Render.allElements[rightWebGallaryIndex3].showSelected++;
+
+}
 
 collectImageToGenerate();
 
-webGallay1.addEventListener('click', handleClicking);
-webGallay2.addEventListener('click', handleClicking);
-webGallay3.addEventListener('click', handleClicking);
+
+secOne.addEventListener('click', handleClicking);
+
 
 function handleClicking(event) {
   count++;
@@ -112,27 +113,28 @@ function handleClicking(event) {
 
     }
     else if (event.target.id === 'webGallary3R') {
+      Render.allElements[rightWebGallaryIndex3].select++;
     }
     console.log(count);
     collectImageToGenerate();
     console.log(Render.allElements);
   } else {
 
-    let Result = document.getElementById('Result');
+
     Result.appendChild(button);
-    webGallay1.removeEventListener('click', handleClicking);
-    webGallay2.removeEventListener('click', handleClicking);
-    webGallay3.removeEventListener('click', handleClicking);
+    secOne.removeEventListener('click', handleClicking);
+
   }
 }
 let button = document.createElement('button');
-button.setAttribute("id", "A");
+Result.appendChild(button);
+button.setAttribute('id', 'A');
 button.textContent = 'View Results';
 
 button.addEventListener('click', lastHandle);
 
 function lastHandle(event) {
-  console.log(event.target.id);
+  console.log(event);
   ulList();
   button.removeEventListener('click', lastHandle);
 }
@@ -140,11 +142,12 @@ function lastHandle(event) {
 
 function ulList() {
 
-  let ul = document.getElementById('unOrderList');
+  let ul = document.getElementById('ulList');
+
   for (let i = 0; i < Render.allElements.length; i++) {
     let li = document.createElement('li');
     ul.appendChild(li);
-    li.textContent = `${Render.allImages[i].name} have these ${Render.allImages[i].select}  and was seen ${Render.allImages[i].showSelected} times `;
+    li.textContent = `The ${Render.allElements[i].name} It has been voted ${Render.allElements[i].select}  and was seen ${Render.allElements[i].showSelected} times `;
   }
 }
 
